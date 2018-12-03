adf988df45efa9662c65ad066a46f0f3dc2d464b
zhangchi <814417245@qq.com>
2018-12-03 19:19:04 +0800

    user1修改后

--- HelloProject/src/Main.java
+++ HelloProject/src/Main.java
@@ -5,5 +5,13 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("aaaa");
+        System.out.println("aaaaa!");
+        System.out.println("ddddd!");
+        System.out.println("ggg!");
+        System.out.println("fff!");
+        System.out.println("ggg!");
+       System.out.println("ggghhhh!");
+        System.out.println("hhhhhh!");
     }
 }
