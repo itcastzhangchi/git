d92004721453303a3e7a36a1493a47e614676cf4
zhangchi <814417245@qq.com>
2018-12-03 19:20:32 +0800

    user2修改后

--- HelloProject/src/Main.java
+++ HelloProject/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
+		
         System.out.println("HelloWorld!");
+        System.out.println("user2222222222222222!");
+        System.out.println("yyyyyyy!");
+        System.out.println("uuuuuuuuuuuuuu!");
+        System.out.println("iiiiiiiiiiiiiii!");
+        System.out.println("wwwwwwwwwwwwwwwwwwww!");
     }
 }
